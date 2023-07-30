f8fa4882be1059149d511eb38471348ecdeb0a37
g-vidhulakripali <g.vidhulakripali@gmail.com>
2023-07-30 23:19:57 +0530

    Update the display

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const display = document.querySelector(".display");
 const startBtn = document.getElementById("startBtn");
 const stopBtn = document.getElementById("stopBtn");
 const resetBtn = document.getElementById("resetBtn");
+
+// Function to update the display with the current timer values
+function updateDisplay() {
+  const displaySeconds = seconds < 10 ? "0" + seconds : seconds;
+  const displayMinutes = minutes < 10 ? "0" + minutes : minutes;
+  const displayHours = hours < 10 ? "0" + hours : hours;
+
+  display.textContent = `${displayHours}:${displayMinutes}:${displaySeconds}`;
+}
