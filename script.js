40e7535a142ade233108ae0cc833fd149753d4db
g-vidhulakripali <g.vidhulakripali@gmail.com>
2023-07-30 23:19:06 +0530

    Html and Css Design and start Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+let timer;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+const display = document.querySelector(".display");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resetBtn = document.getElementById("resetBtn");
